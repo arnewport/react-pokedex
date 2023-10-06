b96f41209f872b3b900b8fcb49aa08e31c67644c
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-10-06 21:01:32 +0200

    Styling Header js and css

--- pokedex-front/src/App.js
+++ pokedex-front/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import Header from './components/Header';
 
 import PokemonTable from "./components/PokemonTable";
 
 function App() {
   return (
     <Router>
+        <Header />
       <main className="container">
         <Routes>
           <Route path="/" element={<PokemonTable />} />
