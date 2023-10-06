edece08582a12c9139698967b931b2f1f99b2115
Andrew <anewport@outlook.com>
2023-10-06 10:30:19 -0400

    Added type display and capitalized names / types

--- pokedex-front/src/components/PokemonTable.js
+++ pokedex-front/src/components/PokemonTable.js
@@ -16,7 +16,7 @@ if (loading) {
                         <th>Picture</th>
                         <th>Japanese Name</th>
                         <th>English Name</th>
-                        <th>Type</th>
+                        <th>Type(s)</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -27,8 +27,8 @@ if (loading) {
                                 <img src={p.sprites.front_default} alt={p.name}></img>
                             </td>
                             <td>{p.japaneseName}</td>
-                            <td>{p.name}</td>
-                            <td>{p.name}</td>
+                            <td>{p.name.charAt(0).toUpperCase() + p.name.substring(1, p.name.length)}</td>
+                            <td>{p.types[0].type.name.charAt(0).toUpperCase() + p.types[0].type.name.substring(1, p.name.length)}</td>
                         </tr>
                     ))}
                 </tbody>
