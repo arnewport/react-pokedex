752f747ec0eaa8ba7c99ccf7bd87178ceba7d115
Andrew <anewport@outlook.com>
2023-10-09 01:32:17 -0400

    merging Jessica's header with weekend branch

---
+++ pokedex-front/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+export default function Header(){
+    
+    return <header className="header">
+        
+        <h1 className="header-title"><span>Pokémon</span></h1>
+        <h6>Andrew Newport ・Rachel Lin ・ Jessica Link</h6>
+        <img className="header-image" src={process.env.PUBLIC_URL + "/pokemon_banner/StarterPokemon.png"} alt="Starter Pokemon"></img>
+        
+        </header>;
+}
